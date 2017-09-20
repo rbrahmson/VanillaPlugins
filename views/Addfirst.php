db699af8f54b666caa86c94ba758ed57181e8d01
Roger Brahmson <rbrahmson@users.noreply.github.com>
2017-09-20 11:20:02 +0300

    Delete Addfirst.php

--- views/Addfirst.php
+++
@@ -1,8 +0,0 @@
-<?php echo $this->Form->Open();
-echo '<div id=FDP><div id=HELP><div ID=xContent>';
-$Closebutton = '<h1><a class="Button ffcolumn  " style="float: right;" href="' . Url('/plugin/feeddiscussionsplus/ListFeeds'). 
-	   '" title="' . t('Return to the configuration screen').'"> <b style="font-size:15px;">☒</b> '.t("Close").'</a></h1>';
-echo $Closebutton;
-include_once "AddfirstGuide.htm";
-echo $Closebutton;
-echo '</div></div></div>';?>
